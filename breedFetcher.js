e1d9031ffbe007f84ea5bbb7149240238c8e5c4e
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-23 19:34:59 -0600

    refactored code and tested using mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,23 @@
 const request = require('request');
-//const apiUrl = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
-let apiUrl = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const breedToSearch = process.argv[2];
-// if no argument is passed
-if (!breedToSearch) {
-  console.error('Please provide a breed name as a command-line argument.');
-  process.exit(1);
-}
 
-apiUrl += `${breedToSearch}`;
+const fetchBreedDescription = function(breedName, callback) {
 
-request(apiUrl, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode); // checking if response exists (is truthy), and if it does, it then accesses the statusCode property of the response object
-  console.log('body:', body);
-  console.log(typeof body);
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(apiUrl, (error, response, body) => {
+   
+    const data = JSON.parse(body);
+  
+    if (error) {
+      callback(error, null);
+    } else if (data.length === 0) {
+      callback('Breed not found. Please try another breed name.', null);
+    } else {
+      // call the callback with null for error and the description from the body
+      callback(null, data[0].description);
+    }
 
-  //convert string to object
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-
-  if (data.length === 0) {
-    console.log('Breed not found. Please try another breed name.');
-  } else {
-    // print description of the first element in the array of objects (i.e., data)
-    console.log(data[0].description);
-  }
+  });
+};
 
 
-});
+module.exports = { fetchBreedDescription };
